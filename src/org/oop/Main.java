3d848aaf4d66a38edac262c34667ce9820768a1b
Ilario Pierbattista <pierbattista.ilario@gmail.com>
2014-12-20 11:00:58 +0100

    Impostazione del tema di sistema

--- src/org/oop/Main.java
+++ src/org/oop/Main.java
@@ -4,11 +4,19 @@ import org.oop.controller.Controller;
 import org.oop.view.Layout;
 import org.oop.view.Mainframe;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         /*Layout mainlayout = new Layout();
 
         new Controller(mainlayout);*/
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ee) {
+            ee.printStackTrace();
+        }
 
         Mainframe mainframe = new Mainframe();
     }
