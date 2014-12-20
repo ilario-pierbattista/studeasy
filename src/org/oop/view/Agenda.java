00b22b0a1271e5bebf5cd2cec0542a95a81f1183
Vittorio Morganti <toioski@MacBook-Air-di-Vittorio.local>
2014-12-20 11:03:09 +0100

    Inizio sviluppo viste agenda

--- src/org/oop/view/Agenda.java
+++ src/org/oop/view/Agenda.java
@@ -1,15 +1,19 @@
 package org.oop.view;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by toioski on 20/12/14.
  */
 public class Agenda {
     public JPanel agendapanel;
-    private JLabel text;
+    private JScrollPane sidebarpanel;
+    private JScrollPane activitypanel;
+    Dimension minimumsize = new Dimension(200,-1);
 
     public Agenda() {
         super();
+        sidebarpanel.setMinimumSize(minimumsize);
     }
 }
