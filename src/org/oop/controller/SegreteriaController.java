d14d673d8be69deeab1388e56b0ec510b7224af5
marco-levantesi <lvns@hotmail.it>
2014-12-26 14:13:44 +0100

    Continuo aggiunta pulsante in segreteria-da chiarire-

--- src/org/oop/controller/SegreteriaController.java
+++ src/org/oop/controller/SegreteriaController.java
@@ -1,5 +1,6 @@
 package org.oop.controller;
 
+import org.oop.view.Mainframe;
 import org.oop.view.Segreteria;
 import java.awt.event.ActionEvent;
 
@@ -20,6 +21,34 @@ public class SegreteriaController {
      * action per completare il form di registrazione utente
      */
     class InsertFormAction extends AbstractAction {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            //il comando seguente è una sorta di esempio, i dati da inserire nel form dovranno essere immessi da tastiera
+            //poi passati come parametri al costruttore nella view di Segreteria
+            //Segreteria formprofilo = new Segreteria(...);
+
+            //per inserire la schermata di riempimento form:
+            //formprofilo.addInsForm();
+
+            Mainframe.refreshView();
+        }
+    }
+
+    /**
+     * Action per annullare l'immissione del form
+     */
+    class quitFormAction extends AbstractAction {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    /**
+     * Action per confermare l'immissione del form
+     */
+    class submitFormAction extends AbstractAction {
         @Override
         public void actionPerformed(ActionEvent e) {
 
--- src/org/oop/view/Segreteria.java
+++ src/org/oop/view/Segreteria.java
@@ -8,14 +8,37 @@ import java.awt.event.ActionListener;
  */
 public class Segreteria extends AbstractView<Segreteria> {
     public JPanel segreteriapanel;
+
+    private JPanel inserisciform;
+    private JButton annullaform;
+    private JButton submitform;
+
     private JLabel text;
+    private JLabel annoiscrizione;
+    private JLabel annocorso;
+    private JLabel creditiacquisiti;
+    private JLabel corsodilaurea;
+    private JLabel statosuperamentoesami;
     private JButton aggiungiform;
 
-    public Segreteria(){
+    public Segreteria() {
         super();
     }
 
+    //costruttore per completare il form di immissione dati anagrafici
+    public Segreteria(String annoiscrizione, String annocorso, String creditiacquisiti, String corsodilaurea, String statosuperamentoesami) {
+        this.annoiscrizione.setText(annoiscrizione);
+        this.annocorso.setText(annocorso);
+        this.creditiacquisiti.setText(creditiacquisiti);
+        this.corsodilaurea.setText(corsodilaurea);
+        this.statosuperamentoesami.setText(statosuperamentoesami);
+    }
+
+
+    public void addInsForm () {segreteriapanel.add(inserisciform);}
     public void insFormButtonListener(ActionListener l) {
         aggiungiform.addActionListener(l);
     }
+
+    public JButton getAggiungiform() { return aggiungiform;}
 }
