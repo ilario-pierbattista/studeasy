d2443f000bb790b41e95958912684fc1893d371f
marco-levantesi <lvns@hotmail.it>
2014-12-27 18:34:37 +0100

    immissione form da segreteria quasi completa

--- src/org/oop/Main.java
+++ src/org/oop/Main.java
@@ -2,6 +2,7 @@ package org.oop;
 
 import org.oop.controller.AgendaController;
 import org.oop.controller.MainController;
+import org.oop.controller.SegreteriaController;
 import org.oop.db.DatabaseConfig;
 import org.oop.model.dao.CorsoDAO;
 import org.oop.model.dao.DocenteDAO;
@@ -11,6 +12,7 @@ import org.oop.model.entities.Docente;
 import org.oop.model.entities.InsegnamentoOfferto;
 import org.oop.test.db.DatabaseManagerTest;
 import org.oop.view.Mainframe;
+import org.oop.view.Segreteria;
 
 import javax.swing.*;
 
@@ -30,6 +32,7 @@ public class Main {
 
         Mainframe mainframe = new Mainframe();
         new AgendaController(mainframe.agenda);
+        new SegreteriaController(mainframe.segreteria);
     }
 
     private static void testDatabase() {
