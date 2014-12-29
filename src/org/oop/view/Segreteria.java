3a80b96ad119a9c9dd836689499bf6aef2518fac
marco-levantesi <lvns@hotmail.it>
2014-12-29 16:57:26 +0100

    inizio segreteria

--- src/org/oop/view/Segreteria.java
+++ src/org/oop/view/Segreteria.java
@@ -8,12 +8,18 @@ import java.awt.event.ActionListener;
  * Created by toioski on 20/12/14.
  */
 public class Segreteria extends AbstractView<Segreteria> {
-
+    public JFrame frame = new JFrame("Segreteria");
     public JPanel segreteriapanel;
+    private JTabbedPane segreteriatabpane;
+    public Agenda agenda = new Agenda();
 
 
     public Segreteria() {
         super();
+        frame.setContentPane(segreteriapanel);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //frame.setVisible(true);
     }
 
 }
