2980d3c2c5a2943147f23b90d7436b4364e37d35
marco-levantesi <lvns@hotmail.it>
2015-01-02 20:24:54 +0100

    Sottosezioni segreteria quasi ultimate

--- src/org/oop/controller/ImmatricolazioneController.java
+++ src/org/oop/controller/ImmatricolazioneController.java
@@ -1,14 +1,11 @@
 package org.oop.controller;
 
+import org.oop.view.AbstractView;
 import org.oop.view.Immatricolazione;
 
 import javax.swing.*;
-import javax.swing.text.DateFormatter;
-import javax.swing.text.DefaultFormatterFactory;
 import java.awt.event.ActionEvent;
-import java.text.DateFormat;
-import java.text.NumberFormat;
-import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static org.oop.general.Utils.*;
 
@@ -24,28 +21,21 @@ public class ImmatricolazioneController {
 
     class SubmitFormAction extends AbstractAction {
         @Override
-        public void actionPerformed(ActionEvent e) throws NumberFormatException {
-            JFormattedTextField datanascita = view.getDatanascita();
-            JFormattedTextField anno = view.getAnnoconseguimento();
-
-            //DateFormat dateformat = new SimpleDateFormat("dd-mm-yyyy");
-            //datanascita.setValue(dateformat);
-            //anno.setValue(dateformat);
+        public void actionPerformed(ActionEvent e)  {
+            Date datanascita = (Date) view.getDatanascita().getValue();
 
             String codicefiscale = view.getCodicefiscale().getText();
             String matricola = view.getMatricola().getText();
-
             //il voto delle superiori si assume in centesimi
             int voto = Integer.parseInt(view.getVoto().getText());
 
-
-
+            String anno = view.getAnnoconseguimento().getText();
             String nome = view.getNome().getText();
             String cognome = view.getCognome().getText();
             String luogonascita = view.getLuogonascita().getText();
             String diploma = view.getDiploma().getText();
 
-            if ( (inputCodiceFiscaleControl(codicefiscale)) && (inputMatricolaControl(matricola) ) && ( (voto<=100) || (voto>=60) ) && (inputNameControl(nome)) && (inputNameControl(cognome)) && inputCorsoLaureaControl(luogonascita) && inputCorsoLaureaControl(diploma) && inputYearControl(anno.getText()) ) {
+            if ( (inputCodiceFiscaleControl(codicefiscale)) && (inputMatricolaControl(matricola) ) && ( (voto<=100) || (voto>=60) ) && (inputNameControl(nome)) && (inputNameControl(cognome)) && inputCorsoLaureaControl(luogonascita) && inputCorsoLaureaControl(diploma) && inputYearControl(anno) ) {
                 String name = stringToCapital(nome);
                 String surname = stringToCapital(cognome);
                 //butta tutto nel modulo
@@ -65,7 +55,7 @@ public class ImmatricolazioneController {
             } else if (!inputCorsoLaureaControl(diploma)) {
                 JOptionPane.showMessageDialog(null,"Diploma Errato!");
             } else {
-                if (!inputYearControl(anno.getText())) {
+                if (!inputYearControl(anno)) {
                     JOptionPane.showMessageDialog(null, "Anno Errato!");
                 } else if ( nome.equals("") || cognome.equals("") || luogonascita.equals("") || diploma.equals("") || codicefiscale.equals("") || matricola.equals("")){
                     JOptionPane.showMessageDialog(null,"Uno o Più Campi devono essere riempiti");
@@ -79,16 +69,15 @@ public class ImmatricolazioneController {
         @Override
         public void actionPerformed(ActionEvent e) {
             //in questo caso l'annullamento del form comporterà la cancellazione di tutti i campi
-            view.getMatricola().setValue(null);
             view.getMatricola().setText("");
-            //view.getNome().setText("");
-            //view.getCognome().setText("");
-            //view.getDatanascita().setText("");
-            //view.getLuogonascita().setText("");
-            //view.getCodicefiscale().setText("");
-            //view.getDiploma().setText("");
-            //view.getVoto().setText("");
-            //view.getAnnoconseguimento().setText("");
+            view.getNome().setText("");
+            view.getCognome().setText("");
+            view.getDatanascita().setText("");
+            view.getLuogonascita().setText("");
+            view.getCodicefiscale().setText("");
+            view.getDiploma().setText("");
+            view.getVoto().setText("");
+            view.getAnnoconseguimento().setText("");
         }
     }
 }
