a41f4637c0feaf27c24afe8a0159b4ca2df197a2
Ilario Pierbattista <pierbattista.ilario@gmail.com>
2015-01-07 11:30:16 +0100

    Fix agenda

--- src/org/oop/model/Agenda.java
+++ src/org/oop/model/Agenda.java
@@ -25,4 +25,15 @@ public class Agenda {
         cicli.add(ciclo);
         return this;
     }
+
+    public Agenda removeCiclo(Ciclo ciclo) {
+        boolean found = false;
+        for (int i = 0; i < cicli.size() && !found; i++) {
+            if(ciclo.getId() == cicli.get(i).getId()) {
+                found = true;
+                cicli.remove(i);
+            }
+        }
+        return this;
+    }
 }
